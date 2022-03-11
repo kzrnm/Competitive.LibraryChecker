8abc6259acde4599055af25e6420980c5afd044b
kzrnm <gengesa@gmail.com>
2022-03-12 03:03:53 +0900

    initial

---
+++ Kzrnm.Competitive.LibraryChecker/CompetitiveSolvers.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Kzrnm.Competitive.LibraryChecker
+{
+    public static class CompetitiveSolvers
+    {
+        /// <summary>
+        /// get <see cref="ICompetitiveSolver"/> classes in <paramref name="assembly"/>.
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<ICompetitiveSolver> GetSolvers(Assembly assembly)
+            => assembly.GetTypes()
+                .OfType<ICompetitiveSolver>();
+
+        /// <summary>
+        /// get <see cref="ICompetitiveSolver"/> class having <paramref name="name"/> in <paramref name="assembly"/>.
+        /// </summary>
+        /// <returns></returns>
+        public static ICompetitiveSolver GetSolver(Assembly assembly, string name)
+            => assembly.GetTypes()
+                .OfType<ICompetitiveSolver>()
+                .Single(s => s.Name == name);
+    }
+}
---
+++ Kzrnm.Competitive.LibraryChecker/ICompetitiveSolver.cs
@@ -0,0 +1,21 @@
+﻿namespace Kzrnm.Competitive.LibraryChecker
+{
+    /// <summary>
+    /// Solver
+    /// </summary>
+    public interface ICompetitiveSolver
+    {
+        /// <summary>
+        /// Solver name
+        /// </summary>
+        string Name { get; }
+        /// <summary>
+        /// Timeout of solver
+        /// </summary>
+        int TimeoutSecond { get; }
+        /// <summary>
+        /// Run program
+        /// </summary>
+        void Solve();
+    }
+}
