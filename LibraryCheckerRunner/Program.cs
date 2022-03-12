9d9806eb1a8ae7dcfba97110e8ad33c3a96e1521
kzrnm <gengesa@gmail.com>
2022-03-12 19:14:50 +0900

    Use 2-SAT

--- LibraryCheckerRunner/Program.cs
+++ LibraryCheckerRunner/Program.cs
@@ -40,20 +40,49 @@ namespace LibraryCheckerRunner
             }
         }
     }
-    public class FloorSumSolver : ICompetitiveSolver
+    public class TwoSatSolver : ICompetitiveSolver
     {
-        public string Name => "sum_of_floor_of_linear";
+        public string Name => "two_sat";
         public double TimeoutSecond => 5;
         public void Solve(Stream inputStream, Stream outputStream)
         {
             var utf8 = new System.Text.UTF8Encoding(false);
             var cr = new ConsoleReader(inputStream, utf8);
             using var cw = new ConsoleWriter(outputStream, utf8);
-            int T = cr;
-            for (int i = 0; i < T; i++)
+            _ = cr.Ascii();
+            _ = cr.Ascii();
+            int n = cr;
+            int m = cr;
+            var twoSat = new TwoSat(n);
+            for (int i = 0; i < m; i++)
             {
-                cw.WriteLine(MathLib.FloorSum(cr, cr, cr, cr));
+                int a = cr;
+                int b = cr;
+                _ = cr.Int();
+
+                int a1 = Math.Abs(a) - 1;
+                bool a2 = a >= 0;
+                int b1 = Math.Abs(b) - 1;
+                bool b2 = b >= 0;
+                twoSat.AddClause(a1, a2, b1, b2);
             }
+            if (twoSat.Satisfiable())
+            {
+                cw.WriteLine("s SATISFIABLE");
+                cw.StreamWriter.Write("v ");
+                var res = new int[n + 1];
+                var answer = twoSat.Answer();
+                for (int i = 0; i < n; i++)
+                {
+                    if (answer[i])
+                        res[i] = i + 1;
+                    else
+                        res[i] = -(i + 1);
+                }
+                cw.WriteLineJoin(res);
+            }
+            else
+                cw.WriteLine("s UNSATISFIABLE");
         }
     }
 }
